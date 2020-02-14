3ab94c06f8de175c526ef02eb143964163d9dd07
PyFellow <50703361+pyfellow@users.noreply.github.com>
2020-02-14 16:29:15 +0600

    bug fixed (auto play)

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@ $('.owl-carousel').owlCarousel({
     margin: 30,
     nav: false,
     autoPlay: true,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
 })
